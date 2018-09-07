7580e5d2086533699c230bf798a8723c22379302
charles <vmxbmx@gmail.com>
2018-09-07 14:53:32 +0800

    added missing config file

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * DB_HOST -> database hostname
+ * DB_NAME -> database name
+ * DB_USER -> database username
+ * DB_PASS -> database user's password
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'syahril_convo');
+define('DB_USER', 'root');
+define('DB_PASS', 'localdbroot');
+
+/**
+ * turn ON error reporting
+ */
+
+ error_reporting(E_ALL);
+
+ 
